f4b6bbedee1c585cfe71332c1669715920dfb93d
Mads Møller Jensen <mmjensen87@gmail.com>
2018-02-20 11:03:04 +0100

    Allow localhost access

--- idservice.js
+++ idservice.js
@@ -14,7 +14,13 @@ const salt = "CIO&vinegar" //placeholder
 
 const allowCrossDomain = function(req, res, next) {
 
-    res.header('Access-Control-Allow-Origin', '*');
+	var allowedOrigins = ['http://127.0.0.1', 'http://localhost', 'null', 'https://cio.cs.au.dk'];
+  	var origin = req.headers.origin;
+  	if(allowedOrigins.indexOf(origin) > -1){
+       res.header('Access-Control-Allow-Origin', origin);
+  	}
+
+    //res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
 
