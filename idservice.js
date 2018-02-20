fbbda37ecaff7b9a8f9ae3f61cdb0219a1b75079
Mads Møller Jensen <mmjensen87@gmail.com>
2018-02-20 10:56:46 +0100

    more debugging

--- idservice.js
+++ idservice.js
@@ -14,13 +14,13 @@ const salt = "CIO&vinegar" //placeholder
 
 const allowCrossDomain = function(req, res, next) {
 
-    res.header('Access-Control-Allow-Origin', 'null');
+    res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
 
 
-    console.log(req.header.origin);
-    
+    console.log(req.header);
+
 
     next();
 }
